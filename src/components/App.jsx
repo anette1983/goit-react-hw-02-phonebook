e4a657eb238533236f68cafd1688d98f5ed1bc9c
Hanna <anna_kon@bigmir.net>
2023-03-11 23:48:54 +0200

    added prettier formatting, HW finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,127 +4,118 @@ import ContactsList from 'components/ContactsList';
 import Filter from 'components/Filter';
 import ContactForm from 'components/ContactForm';
 
-
- class App extends Component {
+class App extends Component {
   state = {
     // contacts: [],
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
 
   getFormData = data => {
     console.log('data :>> ', data);
-    const dataIncludes = this.state.contacts.find(contact => contact.name === data.name);
-    if(dataIncludes) {
+    const dataIncludes = this.state.contacts.find(
+      contact => contact.name === data.name
+    );
+    if (dataIncludes) {
       return alert(`${data.name} is already in contacts`);
     }
     const newData = {
       ...data,
       id: nanoid(),
-    }
+    };
     this.setState(prevState => ({
       contacts: [newData, ...prevState.contacts],
     }));
-  }
+  };
 
-  
-  handleFilterChange = (e) => {
+  handleFilterChange = e => {
     const target = e.target.value;
     this.setState(prevState => ({
       ...prevState,
       filter: target,
     }));
-   
-  }
+  };
 
-
-  handleDelete = (id) => {
+  handleDelete = id => {
     console.log('id :>> ', id);
-   const filtered = this.state.contacts.filter(contact=> contact.id !== id);
-   this.setState(prevState => ({
-    ...prevState,
+    const filtered = this.state.contacts.filter(contact => contact.id !== id);
+    this.setState(prevState => ({
+      ...prevState,
       contacts: filtered,
     }));
-
-  
-
-  } 
+  };
 
   render() {
     return (
       <div>
         <h1>Phonebook</h1>
-        <ContactForm getFormData={this.getFormData}/>
+        <ContactForm getFormData={this.getFormData} />
         <h2>Contacts</h2>
-        <Filter handleFilterChange={this.handleFilterChange} filter={this.state.filter}/>
-        <ContactsList contacts={this.state.contacts} filter={this.state.filter} onClick={this.handleDelete}/>
+        <Filter
+          handleFilterChange={this.handleFilterChange}
+          filter={this.state.filter}
+        />
+        <ContactsList
+          contacts={this.state.contacts}
+          filter={this.state.filter}
+          onClick={this.handleDelete}
+        />
       </div>
     );
   }
 }
 
-
-
 export default App;
 
-
 // handleChange = e => {
-  //   this.setState({ [e.target.name]: e.target.value });
-  //   // [e.currentTarget.name]: e.currentTarget.value якщо декілька
-  // };
+//   this.setState({ [e.target.name]: e.target.value });
+//   // [e.currentTarget.name]: e.currentTarget.value якщо декілька
+// };
 
-  // changeFilter = event => {
-  //   this.setState({
-  //     filter: event.currentTarget.value,
-  //   });
-  // };
+// changeFilter = event => {
+//   this.setState({
+//     filter: event.currentTarget.value,
+//   });
+// };
 
+// handleFilterChange = (e) => {
+//   const target = e.target.value;
+//   this.setState(prevState => ({
+//     filter: target,
+//   }));
+// const filtered = this.filteredContacts();
 
+// this.setState(prevState => ({
+//   contacts: [filtered, ...prevState.contacts],
+// }));
+// console.log('this.state.contacts :>> ', this.state.contacts);
 
+// }
 
-  // handleFilterChange = (e) => {
-  //   const target = e.target.value;
-  //   this.setState(prevState => ({
-  //     filter: target,
-  //   }));
-    // const filtered = this.filteredContacts();
-  
+// changeFilter = event => {
+//   this.setState({
+//     filter: event.currentTarget.value,
+//   });
+// };
 
-    // this.setState(prevState => ({
-    //   contacts: [filtered, ...prevState.contacts],
-    // }));
-    // console.log('this.state.contacts :>> ', this.state.contacts);
+// filterContacts = contactId => {
+//   this.setState(prevState => ({
+//     contacts: prevState.contacts.filter(contact=> contact.id !== contactId),
+//   }));
+// };
 
-  // }
-
-    // changeFilter = event => {
-  //   this.setState({
-  //     filter: event.currentTarget.value,
-  //   });
-  // };
-
-  // filterContacts = contactId => {
-  //   this.setState(prevState => ({
-  //     contacts: prevState.contacts.filter(contact=> contact.id !== contactId),
-  //   }));
-  // };
-
-
-
-  
-
-  // filteredContacts = () => { 
-  //   const normalizedFilter = this.state.filter.toLowerCase();
-  //   console.log('normalizedFilter :>> ', normalizedFilter);
-  //   // чому таке запізнення?
-  //   return this.state.contacts.filter(contact =>
-  //     contact.name
-  //       .toLowerCase()
-  //       .includes(normalizedFilter),
-  //   );
-  // };
+// filteredContacts = () => {
+//   const normalizedFilter = this.state.filter.toLowerCase();
+//   console.log('normalizedFilter :>> ', normalizedFilter);
+//   // чому таке запізнення?
+//   return this.state.contacts.filter(contact =>
+//     contact.name
+//       .toLowerCase()
+//       .includes(normalizedFilter),
+//   );
+// };
--- src/components/ContactForm/ContactForm.styled.js
+++ src/components/ContactForm/ContactForm.styled.js
@@ -1,24 +1,24 @@
 import styled from 'styled-components';
 
 const StyledForm = styled.form`
-width: 300px;
-border: 1px solid black;
-padding: 12px;
-margin: 15px 0;
-/* display: flex;
+  width: 300px;
+  border: 1px solid black;
+  padding: 12px;
+  margin: 15px 0;
+  /* display: flex;
 flex-direction: column;
 justify-content: center;
  */
-& input {
-  padding: 8px;
-  font-family: inherit;
-}
+  & input {
+    padding: 8px;
+    font-family: inherit;
+  }
 
-& label {
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 8px;
-}
-`
+  & label {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 8px;
+  }
+`;
 
 export default StyledForm;
